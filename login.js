9edb2fdfd990a03e21c66840501ad303a1f69bb7
Matt Kim <matthewjkim@gmail.com>
2017-01-17 17:52:52 -0500

    update login

--- login.js
+++ login.js
@@ -1,12 +1,4 @@
-/* global firebase moment */
-// Steps to complete:
-
-// 1. Initialize Firebase
-// 2. Create button for adding new employees - then update the html + update the database
-// 3. Create a way to retrieve employees from the employee database.
-// 4. Create a way to calculate the months worked. Using difference between start and current time.
-//    Then use moment.js formatting to set difference in months.
-// 5. Calculate Total billed
+console.log('login.js');
 
 // 1. Initialize Firebase
   var config = {
@@ -44,7 +36,7 @@ ui.start('#firebaseui-auth-container', uiConfig);
           if (user) {
             window.location.url = '/index.html';
           } else {
-            
+
             // // User is signed out.
             // document.getElementById('sign-in-status').textContent = 'Signed out';
             // document.getElementById('sign-in').textContent = 'Sign in';
