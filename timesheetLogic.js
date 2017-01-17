7346583ddfc3b579e20371c2497fc5c783b9264a
Matt Kim <matthewjkim@gmail.com>
2017-01-17 16:29:38 -0500

    update

--- timesheetLogic.js
+++ timesheetLogic.js
@@ -100,7 +100,7 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
         signInSuccessUrl: '<url-to-redirect-to-on-success>',
         signInOptions: [
           // Leave the lines as is for the providers you want to offer your users.
-          firebase.auth.EmailAuthProvider.PROVIDER_ID
+          firebase.auth.TwitterAuthProvider.PROVIDER_ID,
         ],
         // Terms of service url.
         tosUrl: '<your-tos-url>'
