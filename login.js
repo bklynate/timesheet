5c04cc9f030e79a872123217d8f1e31d90549645
Matt Kim <matthewjkim@gmail.com>
2017-01-17 18:05:34 -0500

    build

--- login.js
+++ login.js
@@ -28,7 +28,7 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
 // The start method will wait until the DOM is loaded.
 ui.start('#firebaseui-auth-container', uiConfig);
 
-
+console.log('in here');
 
      initApp = function() {
         firebase.auth().onAuthStateChanged(function(user) {
